464ab045e2243fdafc6c91cb0cec493330f70555
melwarebytes <merullshah25@gmail.com>
2025-06-09 02:15:01 +0530

    Add Page added - Yet to complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import {useState} from 'react';
 import PlantGrowthTracker from './components/PlantGrowthTracker';
+import AddPlant from './components/AddPlant'
+import './App.css';
 
 function App() {
-  return <PlantGrowthTracker />;
+  const[page, setPage] = useState('dashboard');
+  return(
+    <div>
+        <nav className='navbar'>
+            <button onClick={() => setPage('dashboard')}>Plant Dashbord</button>
+            <button onClick={() => setPage('add')}>Add Plant</button>
+        </nav>
+        {page === 'dashboard' ? <PlantGrowthTracker /> : <AddPlant />}
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/AddPlant.jsx
@@ -0,0 +1,8 @@
+export default function AddPlant() {
+  return (
+    <div className="add-plant">
+      <h1>Add a New Plant</h1>
+      <p>(Form coming soon!)</p>
+    </div>
+  );
+}
